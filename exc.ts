4c1818602565864aad6209cbd94c11302d64016a
Abedneg12 <yosua12.abednego@gmail.com>
2025-01-24 11:49:57 +0700

    init

---
+++ exc.ts
@@ -0,0 +1,11 @@
+//exercise
+
+// no 1 Write a code to display the multiplication table of a given integer 
+const number: number = 5;
+for (let i: number = 1; i <= 10; i++){
+    console.log(`${number} x ${i} = ${number * i}`);
+}
+
+// no 2 Write a code to check whether a string is a palindrome or not
+let message: string = "quit"
+
